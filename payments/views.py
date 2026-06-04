23ef124cd58a6ddc45f322165370cd1e053f88c7
Samuel-Adisi <samueladisi734@gmail.com>
2026-06-03 18:22:33 -0700

    rep bug fixed

--- payments/views.py
+++ payments/views.py
@@ -175,7 +175,8 @@ class MyPaymentsView(generics.ListAPIView):
 class RepPaymentsView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class   = PaymentSerializer
+
     def get_queryset(self):
         return Payment.objects.filter(
-            handout__course__rep=self.request.user
+            course__rep=self.request.user
         ).order_by("-created_at")
