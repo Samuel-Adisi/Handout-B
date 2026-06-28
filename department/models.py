913312aaf5018aed616d6dbc9b38a5ad600fafd6
Samuel-Adisi <samueladisi734@gmail.com>
2026-06-27 18:51:35 -0700

    user circualr

--- department/models.py
+++ department/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 
-class Department(models.Model):
+"""class Department(models.Model):
     DEPARTMENT_TYPE = [("hnd", "HND"),
                        ("btech", "BTECH")
                        ]
     name = models.CharField(max_length=100, blank=False,null=False)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     department_type = models. CharField(choices=DEPARTMENT_TYPE, blank=False)
     created_by = models.ForeignKey(User , on_delete=models.CASCADE)
     is_active = models.BooleanField(default=False)
@@ -18,3 +20,4 @@ class Department(models.Model):
     
     def __str__(self):
         return F"{self.name}"
+"""
